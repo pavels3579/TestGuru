0ab810d5fd6bbfa181a37c8943c1eafe7c70df21
pavels3579 <pavels357@yandex.ru>
2018-12-21 10:56:29 +0400

    update 1 task 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,30 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-categorirs = Categorie.create([{ title: 'HTML' },
-                              { title: 'Ruby' }
+categories = Categorie.create!([{ title: 'HTML' },
+                              { title: 'Ruby' },
                               { title: 'php' }])
 
-test1 = Test.create(title: 'Ruby 1',
+test1 = Test.create!(title: 'Ruby 1',
                     level: 1,
-                    category_id: categorirs.find_by(title: 'Ruby').id)
+                    category_id: categories.find_by(title: 'Ruby').id)
 
-test2 = Test.create(title: 'Ruby 2',
+test2 = Test.create!(title: 'Ruby 2',
                     level: 2,
-                    category_id: categorirs.find_by(title: 'Ruby').id)
+                    category_id: categories.find_by(title: 'Ruby').id)
 
-q1 = Question.create(body: 'How do you create a new hash?',
+q1 = Question.create!(body: 'How do you create a new hash?',
                     test_id: test1.id)
 
-q1 = Question.create(body: 'How do you create a new array?',
+q1 = Question.create!(body: 'How do you create a new array?',
                     test_id: test1.id)
 
-a1 = Answer.create(body: 'I have to use {}',
-                    correct: true
+a1 = Answer.create!(body: 'I have to use {}',
+                    correct: true,
                     question_id: q1.id)
 
-user1 = User.create(name: 'Mike')
+user1 = User.create!(name: 'Mike')
 
-ut = UserTest.create(user_id: user1.id
+ut = UserTest.create!(user_id: user1.id,
                     test_id: test1.id)
 
