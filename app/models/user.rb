915d37c8a22df3741d16e6f8590a3789bfee97a1
pavels3579 <pavels357@yandex.ru>
2018-12-21 17:20:15 +0400

    update 4 task 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def select_by_level(level)
-    TEST.joins('INNER JOIN user_tests ON tests.id = user_tests.test_id').
-    where("user_id = ? AND level = ?", id, level)
+    TEST.joins('INNER JOIN user_tests ON tests.id = user_tests.test_id')
+        .where("user_id = ? AND level = ?", id, level)
   end
 end
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,3 +1,3 @@
 module TestGuru
-  VERSION = '0.4.3'.freeze
+  VERSION = '0.4.4'.freeze
 end
