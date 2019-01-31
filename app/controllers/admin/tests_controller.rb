da7790c731996bad015186a2bb559b0c175d989c
pavels3579 <pavels357@yandex.ru>
2019-01-31 22:50:13 +0400

    update 2 task 11

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -15,7 +15,7 @@ class Admin::TestsController < Admin::BaseController
   def edit; end
 
   def create
-    @test = current_user.tests.new(test_params)
+    @test = current_user.authored_tests.new(test_params)
 
     if @test.save
       redirect_to [:admin, @test]
