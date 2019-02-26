2cbaebe03c5d4233416db0897411ebdb062788da
pavels3579 <pavels357@yandex.ru>
2019-02-27 00:35:27 +0400

    task 17_2

---
+++ app/controllers/feedbacks_controller.rb
@@ -0,0 +1,22 @@
+class FeedBacksController < ApplicationController
+  respond_to :html
+
+  def new
+    @feedback = Feedback.new
+  end
+
+  def create
+    @feedback = Feedback.new(feedback_params)
+    @feedback.user = current_user.email
+    @feedback.deliver
+    respond_with @feedback, location: root_path
+  end
+
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:title, :body)
+  end
+
+end
