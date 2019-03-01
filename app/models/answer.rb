c90f43dea2a3013796f73f2a4b3e239096f7f896
pavels3579 <pavels357@yandex.ru>
2019-03-01 22:52:23 +0400

    task 17_6

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,11 @@ class Answer < ApplicationRecord
   scope :correct, -> { where(correct: true) }
 
   validates :body, presence: true
+  validates :validate_answers_count, on: :create
+
+  private
+
+  def validate_answers_count
+    errors.add(:answers) if question.answers.count == 4
+  end
 end
