aa199c00834ef51c9ecd22ea25f48370e5b42517
pavels3579 <pavels357@yandex.ru>
2019-03-17 00:43:06 +0400

    update 4 task 18.1

--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -4,58 +4,30 @@ class BadgeService
     @test_passage = test_passage
     @user = test_passage.user
     @test = test_passage.test
-    @badge = nil
-    @badges = []
   end
 
   def try_get_badge
-    badges = Badge.all
+    return [] unless @test_passage.success
 
-    badges.each do |b|
-      @badge = b
-      send(b.rule_name, b.rule_value)
-    end
-    @badges
+    Badge.all.select { |badge| send(badge.rule_name, badge.rule_value) }
   end
 
   def category(rule_value)
-    if @test.category.title = rule_value
-      tests_count = Test.select_by_category(rule_value).count
-      #user_tests_count = @user.tests.select_by_category(rule_value).where(success: true).count
-      user_tests_count = @user.tests.select_by_category(rule_value).where('test_passages.success = ?', true).count
+    return false unless @test.category.title = rule_value
 
-      if tests_count == user_tests_count
-        #give_badge
-        @badges << @badge
-        #give_badge(Badge.first)
-      end
-    end
+    user_successfull_tests.where(category_id: @test.category_id).uniq.count == Test.select_by_category(@test.category.title).count
   end
 
   def level(rule_value)
-    tests_count = Test.select_by_level(rule_value.to_i).count
-    #user_tests_count = @user.tests.select_by_level(rule_value).where(success: true).count
-    #user_tests_count = TestPassage.where(user_id: @user.id, success: true).preload(:test).select_by_level(rule_value.to_i).count
-    #user_tests_count = TestPassage.where(user_id: @user.id, success: true).joins(Test.select_by_level(rule_value)).count
-    user_tests_count = TestPassage.where(user_id: @user.id, success: true).joins(:test).where('tests.level = ?', rule_value.to_i).uniq.count
-
-    if tests_count == user_tests_count
-      #give_badge
-      @badges << @badge
-    end
+    user_successfull_tests.where(level: @test.level).uniq.count == Test.select_by_level(rule_value.to_i).count
   end
 
   def success_first_try(rule_value)
-    #give_badge if (@user.tests.where(test_id: @test.id).count == 1 && @user.tests.where(test_id: @test.id, success: true).count == 1)
-    #give_badge if (TestPassage.where(test_id: @test.id, user_id: @user.id).count == 1 && TestPassage.where(test_id: @test.id, user_id: @user.id, success: true).count == 1)
-    @badges << @badge if (TestPassage.where(test_id: @test.id, user_id: @user.id).count == 1 && TestPassage.where(test_id: @test.id, user_id: @user.id, success: true).count == 1)
+    user_successfull_tests.where(id: @test.id).count == 1
   end
 
-  def give_badge
-    medal = UserBadge.new
-    medal.user = @user
-    medal.badge = @badge
-    medal.save!
+  def user_successfull_tests
+   @user.tests.merge(TestPassage.where(success: true))
   end
 
 end
